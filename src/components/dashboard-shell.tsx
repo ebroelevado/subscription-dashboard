1ed1ec2059e367d11d3a78562d138782d0545f73
Pera Mató <pera@peramato.dev>
2026-03-12 01:03:02 +0100

    style(sidebar): reduce user menu margins

--- src/components/dashboard-shell.tsx
+++ src/components/dashboard-shell.tsx
@@ -442,7 +442,7 @@ export function DashboardShell({
                 <div className="px-4 py-2">
                   <MobileNavLinks onNavigate={() => setSheetOpen(false)} />
                 </div>
-                <div className="px-4 pb-8 border-t mt-auto pt-6">
+                <div className="px-4 pb-4 border-t mt-auto pt-4">
                   <MobileUserMenu />
                 </div>
               </div>
